3d8ba8eda6ebeabb292d9cb95efbdda63704f630
stardvst <lilia.manukyan@hotmail.com>
2025-11-03 21:11:00 +0400

    singleton - dangling ref., 1

---
+++ 6-singletons/4-simple-singleton-dangling/simple-singleton-dangling.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+
+class Singleton
+{
+public:
+	static Singleton &getInstance()
+	{
+		if (!instance)
+		{
+			if (destroyed)
+				onDestroyedReference();
+			else
+				createInstance();
+		}
+		return *instance;
+	}
+
+	Singleton(const Singleton &) = delete;
+
+protected:
+	Singleton() = default;
+
+	virtual ~Singleton()
+	{
+		instance = nullptr;
+		destroyed = true;
+	}
+
+private:
+	static void createInstance()
+	{
+		static Singleton localInstance;
+		instance = &localInstance;
+	}
+
+	static void onDestroyedReference()
+	{
+		throw std::runtime_error("Reference to destroyed singleton");
+	}
+
+	static Singleton *instance;
+	static bool destroyed;
+};
+
+Singleton *Singleton::instance = nullptr;
+bool Singleton::destroyed = false;
+
+class Logger : public Singleton
+{
+public:
+	void log(const std::string &message)
+	{
+		std::cout << "Log: " << message << "\n";
+	}
+};
+
+class Displayer : public Singleton
+{
+public:
+	Displayer()
+	{
+		static_cast<Logger &>(Singleton::getInstance()).log("Displayer construction failed");
+	}
+};
+
+class KeyboardHandler : public Singleton
+{
+public:
+	~KeyboardHandler()
+	{
+		try
+		{
+			static_cast<Logger &>(Singleton::getInstance()).log("KeyboardHandler destruction failed"); // not printed
+		}
+		catch (const std::exception &e)
+		{
+			std::cerr << "Failed to log during KeyboardHandler destruction: " << e.what() << '\n'; // printed
+		}
+	}
+};
+
+int main()
+{
+	KeyboardHandler kb;
+	Displayer dp;
+
+	return 0;
+}
